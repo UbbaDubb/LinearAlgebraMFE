0c629a4783df331c273a77740e8e9254d61f5ff5
Hubert Kasinski <66747361+UbbaDubb@users.noreply.github.com>
2025-03-19 16:31:55 +0000

    Q13i

--- CH1.py
+++ CH1.py
@@ -7,4 +7,14 @@ file_path = "data/indeces-jul26-aug9-2012.xlsx"
 df = pd.read_excel(file_path, engine="openpyxl")  # Specify openpyxl for .xlsx files
 
 # Display the first few rows
-print(df.head())
+print(df)
+
+returns = pd.DataFrame()
+
+returns['DJ_%'] = df['Dow Jones'].pct_change()
+returns['NASDAQ_%'] = df['NASDAQ '].pct_change()
+returns['S&P500_%'] = df['S&P 500'].pct_change()
+
+returns = returns.iloc[1:].reset_index(drop=True)
+
+print(returns)

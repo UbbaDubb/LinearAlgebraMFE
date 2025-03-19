f1a4d31e80d46b5e93ebbb20c16176648cf7fd2d
Hubert Kasinski <66747361+UbbaDubb@users.noreply.github.com>
2025-03-19 13:21:45 +0000

    managed to get it working

--- CH1.py
+++ CH1.py
@@ -1,1 +1,10 @@
-print ("Hello Worlds")
+import pandas as pd
+
+# Define file path
+file_path = "data/indeces-jul26-aug9-2012.xlsx"
+
+# Read the Excel file
+df = pd.read_excel(file_path, engine="openpyxl")  # Specify openpyxl for .xlsx files
+
+# Display the first few rows
+print(df.head())

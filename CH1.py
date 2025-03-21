cb5fa205f76408c2287f336e38f70be64b43b854
Hubert Kasinski <hubertkasinski17@gmail.com>
2025-03-21 19:39:24 +0000

    Completed more questions

--- CH1.py
+++ CH1.py
@@ -1,20 +1,68 @@
 import pandas as pd
+import numpy as np
 
 # Define file path
 file_path = "data/indeces-jul26-aug9-2012.xlsx"
 
 # Read the Excel file
-df = pd.read_excel(file_path, engine="openpyxl")  # Specify openpyxl for .xlsx files
+prices = pd.read_excel(file_path, engine="openpyxl")  # Specify openpyxl for .xlsx files
 
 # Display the first few rows
-print(df)
+print(prices)
 
 returns = pd.DataFrame()
 
-returns['DJ_%'] = df['Dow Jones'].pct_change()
-returns['NASDAQ_%'] = df['NASDAQ '].pct_change()
-returns['S&P500_%'] = df['S&P 500'].pct_change()
+returns['DJ_%'] = prices['Dow Jones'].pct_change()
+returns['NASDAQ_%'] = prices['NASDAQ '].pct_change()
+returns['S&P500_%'] = prices['S&P 500'].pct_change()
 
 returns = returns.iloc[1:].reset_index(drop=True)
 
 print(returns)
+
+returns_mat = returns.to_numpy() #TX
+# Compute mean for centering
+mean_vec = np.mean(returns_mat, axis=0)  # Compute column means
+returns_centered = returns_mat - mean_vec  # Subtract means from values in each column
+
+# Transpose
+returns_mat_t = returns_centered.T
+
+# Correct covariance formula
+cov = (1 / (len(returns_mat) - 1)) * (returns_mat_t @ returns_centered)
+
+print("\nCovariance Matrix (Manual Computation):\n", cov)
+
+cov_matrix = returns.cov()
+print("\nCovariance Matrix:\n", cov_matrix)
+
+log_returns = np.log(prices.iloc[:, 1:] / prices.iloc[:, 1:].shift(1))
+log_returns = log_returns.iloc[1:].reset_index(drop=True)
+print("\n",log_returns)
+
+log_returns_mat = log_returns.to_numpy() #LX
+# Compute mean for centering
+log_mean_vec = np.mean(log_returns_mat, axis=0) #Compute column means
+logrets_centred = log_returns_mat - log_mean_vec # Subtract means from values in each column
+
+#Transpose
+log_returns_mat_t = log_returns_mat.T
+
+#Covariance
+log_cov = (1/(len(log_returns_mat)-1)) * (log_returns_mat_t @ log_returns_mat)
+
+print("\nCovariance MAtrix for Log Returns (Manual):\n", log_cov)
+
+log_cov_mat = log_returns.cov()
+print("Covariaqnce Matrix for Log Returns (Pandas):\n", log_cov_mat)
+
+#std_devs = np.sqrt(np.diag(cov))
+#D_inv = np.linalg.inv(np.diag(std_devs))
+#corr_matrix = D_inv @ cov @ D_inv
+#print("Correlation Matrix:\n", corr_matrix)
+
+#corr_matrix = returns.corr()
+#print("Correlation Matrix:\n", corr_matrix)
+
+#corr_matrix_np = np.corrcoef(returns_mat, rowvar=False)
+#print("Correlation Matrix (NumPy):\n", corr_matrix_np)

3f39c6f770297da371b5b5968366a75b030368c0
Hubert Kasinski <hubertkasinski17@gmail.com>
2025-04-30 17:16:56 +0100

    forward substitution

--- CH2.py
+++ CH2.py
@@ -1,2 +1,52 @@
 import numpy as np
 import pandas as pd
+import mfekit as mfk
+
+from scipy.linalg import lu
+
+# Define the matrix
+A = np.array([
+    [2, -1, 0, 1],
+    [-2, 0, 1, -1],
+    [4, -1, 0, 1],
+    [4, -3, 0, 2]
+])
+
+# Perform LU decomposition with partial pivoting: PA = LU
+P, L, U = lu(A)
+
+print(P, "\n",  L, "\n", U)
+
+B = np.array([
+    [2, 0, 0],
+    [4, 5, 0],
+    [1,3, 7]
+    ])
+
+b = np.array([
+    [6],
+    [19],
+    [31]
+])
+
+ans = mfk.forward_susbt(B,b)
+
+cash_flows = np.array([
+    [100, 0, 0, 0],
+    [6, 106, 0, 0],
+    [8, 8, 108, 0],
+    [5, 5, 5, 105]
+])
+
+prices  = np.array([
+    [98],
+    [104],
+    [111],
+    [102]
+])
+
+discount_factors = mfk.forward_susbt(cash_flows, prices)
+print(discount_factors)
+
+rates = mfk.zero_rates(cash_flows, prices)
+print(rates)
--- mfekit.py
+++ mfekit.py
@@ -90,3 +90,69 @@ def corr(df):
     corr_matrix = D_inv @ cov_matrix @ D_inv
 
     return corr_matrix
+
+def forward_susbt(L, b):
+    """
+    Forward substitution to solve Lx = b.
+    
+    Parameters:
+    L (np.ndarray): Lower triangular matrix of size n x n
+    b (np.ndarray): column vector of size n
+    
+    Returns:
+    np.ndarray: Solution vector x.
+    """
+    n = len(b)
+    x = np.zeros(n)
+    
+    for i in range(n):
+        x[i] = (b[i] - np.dot(L[i, :i], x[:i])) / L[i, i]
+    
+    # Return as a column vector
+    x = x.reshape(-1, 1)
+    
+    return x
+
+def forward_subst_bidiag(L,b):
+    """
+    Forward substitution to solve Lx = b for a bidiagonal matrix L.
+    
+    Parameters:
+    L (np.ndarray): Bidiagonal matrix of size n x n
+    b (np.ndarray): column vector of size n
+    
+    Returns:
+    np.ndarray: Solution vector x.
+    """
+    n = len(b)
+    x = np.zeros(n)
+    
+    # Forward substitution
+    for i in range(n):
+        if i == 0:
+            x[i] = b[i] / L[i, i]
+        else:
+            x[i] = (b[i] - L[i, i-1] * x[i-1]) / L[i, i]
+    
+    # Return as a column vector
+    x = x.reshape(-1, 1)
+    
+    return x
+
+def zero_rates(cash_flows, prices):
+    """
+    Calculate zero rates from cash flows and prices.
+    
+    Parameters:
+    cash_flows (np.ndarray): Cash flow matrix of size n x m
+    prices (np.ndarray): Price vector of size n x 1
+    
+    Returns:
+    np.ndarray: Zero rates vector of size m x 1
+    """
+    discount_factors = forward_subst_bidiag(cash_flows, prices)
+    zero_rates = np.zeros(cash_flows.shape[1])
+    for i in range(cash_flows.shape[1]):
+        zero_rates[i] = (-np.log(discount_factors[i]))/(i+1)
+    
+    return zero_rates
